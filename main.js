897071d478b8a443958f90f3b1265489937f29c5
Vladislav Pavlov (ISh-) <imwaves@gmail.com>
2024-08-01 22:10:38 +0400

    contacts, scrool loop, nav, some hovers

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('YAHE!')
+const $doc = document.documentElement;
+
+document.body.appendChild(
+  document.querySelector('.Intro').cloneNode(true),
+);
+
+document.addEventListener('scroll', e => {
+  const { scrollTop, scrollHeight, clientHeight } = $doc;
+  if (scrollTop + clientHeight >= scrollHeight)
+    $doc.scrollTop = 0;
+});
