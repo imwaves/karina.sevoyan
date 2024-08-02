8c5663a5794376bb573a6c68be108fe47eaaa69c
Vladislav Pavlov (ISh-) <imwaves@gmail.com>
2024-08-02 18:43:58 +0400

    upgrade

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 const $doc = document.documentElement;
 
-document.body.appendChild(
-  document.querySelector('.Intro').cloneNode(true),
-);
+// document.body.appendChild(
+//   document.querySelector('.Intro').cloneNode(true),
+// );
 
 document.addEventListener('scroll', e => {
-  const { scrollTop, scrollHeight, clientHeight } = $doc;
-  if (scrollTop + clientHeight >= scrollHeight)
-    $doc.scrollTop = 0;
+  // const { scrollTop, scrollHeight, clientHeight } = $doc;
+  // if (scrollTop + clientHeight >= scrollHeight)
+  //   $doc.scrollTop = 0;
+  const $name = document.querySelector('.Name');
+  $name.style.top = Math.max(0, 128 - $doc.scrollTop) + 'px';
+  $name.style.setProperty('--bg-opaq', $doc.scrollTop > window.innerHeight ? .7 : 0.);
 });
+
+window.addEventListener('hashchange', scrollByHash);
+scrollByHash();
+
+function scrollByHash () {
+  console.log(location.hash);
+  if (location.hash) {
+    setTimeout(() => {
+      document.querySelector(location.hash).scrollIntoView({ behavior: 'smooth' });
+    }, 300);
+  }
+}
