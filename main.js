10c6e842df38afea1f2c2e59ddffcd348c7be8f2
Vladislav Pavlov (ISh-) <imwaves@gmail.com>
2024-08-03 11:32:53 +0400

    top pic and more

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
+import headerBgSrc from './assets/pics/preview/IMG_2700.jpg';
+
 const $doc = document.documentElement;
 
-// document.body.appendChild(
-//   document.querySelector('.Intro').cloneNode(true),
-// );
+// show header bg smoothly
+const $headerBg = document.querySelector('.Intro .__bg');
+$headerBg.style.opacity = 0.;
+const headerBgImg = new Image();
 
+Promise.all([
+  new Promise (r => setTimeout(r, 300)),
+  new Promise (r => {
+    headerBgImg.onload = r;
+  }),
+]).then(() => {
+  $headerBg.style.transition = 'opacity .5s';
+  $headerBg.style.opacity = 1.;
+});
+headerBgImg.src = headerBgSrc;
+
+const $name = document.querySelector('.Name');
+$name.style.position = 'fixed'; // progressive improvement if js
 document.addEventListener('scroll', e => {
-  // const { scrollTop, scrollHeight, clientHeight } = $doc;
-  // if (scrollTop + clientHeight >= scrollHeight)
-  //   $doc.scrollTop = 0;
-  const $name = document.querySelector('.Name');
-  $name.style.top = Math.max(0, 128 - $doc.scrollTop) + 'px';
-  $name.style.setProperty('--bg-opaq', $doc.scrollTop > window.innerHeight ? .7 : 0.);
+  $name.style.top = Math.max(0, window.innerHeight/2 - $doc.scrollTop) + 'px';
 });
 
+// scroll and show by hash
 window.addEventListener('hashchange', scrollByHash);
 scrollByHash();
 
 function scrollByHash () {
-  console.log(location.hash);
   if (location.hash) {
     setTimeout(() => {
       document.querySelector(location.hash).scrollIntoView({ behavior: 'smooth' });
